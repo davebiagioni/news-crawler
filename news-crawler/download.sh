1dbfb427f5149c0accbe0326979056c99fec55e6
Dave Biagioni <davebiagioni@gmail.com>
2017-04-11 08:06:29 -0600

    make download script less machine dependent

--- news-crawler/download.sh
+++ news-crawler/download.sh
@@ -1,1 +1,4 @@
-rsync -avP ubuntu@ec2-54-165-0-32.compute-1.amazonaws.com:/home/ubuntu/news-crawler/data/* /Users/davidbiagioni/gitrepos/news-crawler/data/.
+#!/bin/bash
+DATADIR=$HOME/gitrepos/news-crawler/data
+mkdir -p $DATADIR
+rsync -avP ubuntu@ec2-54-165-0-32.compute-1.amazonaws.com:/home/ubuntu/news-crawler/data/* $DATADIR/.
